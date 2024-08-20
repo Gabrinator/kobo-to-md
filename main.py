7bec9732980d8fb8da33e937c0623d72f37c8e5b
Gabrinator <59077391+Gabrinator@users.noreply.github.com>
2024-08-20 14:05:56 -0400

    added docstrings

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import os
 
 # Get the list of Books for which there are highlights
 def get_books(con):
+    """Retrieves a list of books with their corresponding number of highlights from the database.
+
+    Args:
+        con (sqlite3.Connection): An SQLite Database Connection.
+
+    Returns:
+        A list of tuples, where each tuple represents a book and contains:
+            - volumeId: The unique identifier of the book (str).
+            - BookTitle: The title of the book (str).
+            - count(b.BookmarkID): The number of highlights for the book (int).
+    """
+    
     cur = con.cursor()
     res = cur.execute('SELECT b.volumeId, c.BookTitle, count(b.BookmarkID) FROM Bookmark b LEFT JOIN content c on b.ContentID = c.ContentId GROUP BY b.volumeId;')
     books = res.fetchall()
@@ -12,6 +24,18 @@ def get_books(con):
     return books
 
 def get_book_id(books):
+    """Prompts the user to select a book by its ID
+
+    Args:
+        books (list): A list of books, where each book is represented by a tuple containing:
+            - volumeID (str)
+            - BookTitle (str)
+            - highlightCount (int)
+
+    Returns:
+        int: The ID of the selected book
+    """
+
     while True:
         try:
             bookID = int(input("Enter the ID of the book for which you would like to export highlights: "))
