460d50715916a98716781ccffeec92074a9d1a05
Gabrinator <59077391+Gabrinator@users.noreply.github.com>
2024-08-19 21:18:00 -0400

    basic export to markdown

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # kobo-to-md
- Takes Kobo Highlights and exports them to a markdown file
+ Takes Kobo Highlights and exports them to a markdown file.
+
+ ## Usage
+ 1. Navigate to the folder containing KoboReader.sqlite
+ 2. Run the program. It will list the available books with highlights
+ 3. Type in the ID of the book for which you would like to export highlights
+ 4. This will create a markdown file named after the book title in the same directory.
+
+ ## WIP
+ * Add error handling, especially for user input but also file and db
+ * Commandline arguments
+    * config (+ config file for default db and export locations)
+    * list - to list available books
+    * specify output file name
+    * specify input file name
---
+++ main.py
@@ -0,0 +1,52 @@
+import sqlite3
+from tabulate import tabulate
+from unidecode import unidecode
+
+# Get the list of Books for which there are highlights
+def get_books(con):
+    cur = con.cursor()
+    res = cur.execute('SELECT b.volumeId, c.BookTitle, count(b.BookmarkID) FROM Bookmark b LEFT JOIN content c on b.ContentID = c.ContentId GROUP BY b.volumeId;')
+    books = res.fetchall()
+    print(tabulate(books, headers=['ID','VolumeID', 'BookTitle', 'No. of Highlights'], showindex=True, tablefmt='grid', maxcolwidths=[35,35, None]))
+    return books
+
+def to_markdown(con, books, bookID):
+
+    title = books[bookID][1]
+    if title == None:
+        title= input("What should the title of the book be?")
+
+    # Create file
+    f = open(title + '.md', 'x')
+    f.write('# ' + title + '\n') # Write the Title of the book
+
+    # get the highlights
+    cur = con.cursor()
+    res = cur.execute("""SELECT c.Title as Chapter, Text , b.ChapterProgress
+FROM Bookmark b LEFT JOIN content c ON b.ContentId=c.ContentId 
+WHERE volumeId='""" + books[bookID][0] + """' ORDER BY Chapter ASC, b.ChapterProgress ASC;""")
+    highlights=res.fetchall()
+    
+    # write the different quotes
+    currentChapter = highlights[0][0]
+    f.write('## ' + currentChapter +'\n')
+    for row in highlights:
+        if row[0] != currentChapter:
+            currentChapter=row[0]
+            f.write('## ' + currentChapter + '\n')
+
+        f.write("> " + unidecode(row[1]) + "\n\n")
+    f.close()
+
+
+con = sqlite3.connect('KoboReader.sqlite') # default filename
+
+print("Welcome to Kobo to Markdown")
+print("The following books have highlights:")
+
+books = get_books(con)
+bookID = int(input("Enter the ID of the book for which you would like to export highlights: "))
+to_markdown(con, books, bookID)
+
+# Close the DB connnection
+con.close()
